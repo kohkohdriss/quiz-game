ab8037cfb672d78a34feabed5e28fe1803ea3f6c
driss <kohkoh.driss@gmail.com>
2023-01-12 11:58:48 +0100

    création de composant ScoreBoard
    ce composant va gérer la partie de score entre le Player et l'ordinateur

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,71 @@
 <template>
-  <div v-if="this.question">
-    <h1 v-html="this.question"></h1>
+  <div>
+    <ScoreBoard />
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
 
-    <template v-for="(answer, index) in this.answers" v-bind:key="index">
-      <input
-        id="index"
-        type="radio"
-        name="options"
-        :value="answer"
-        v-model="this.chosen_answer"
-      />
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input
+          id="index"
+          type="radio"
+          name="options"
+          :value="answer"
+          v-model="this.chosenAnswer"
+          :disabled="this.answerSubmitted"
+        />
 
-      <label v-html="answer" for="${answer + index}"></label><br />
+        <label v-html="answer"></label><br />
+      </template>
+
+      <button
+        v-if="!this.answerSubmitted"
+        @click="this.submitAnswer()"
+        class="send"
+        type="button"
+      >
+        Send
+      </button>
+
+      <section v-if="this.answerSubmitted" class="result">
+        <h4
+          v-if="this.chosenAnswer == this.correctAnswer"
+          v-html="
+            '&#9989; Congratulations, the answer ' +
+            this.correctAnswer +
+            ' is correct.'
+          "
+        ></h4>
+        <h4
+          v-else
+          v-html="
+            '&#10060; I\'m sorry, you picked the wrong answer. The correct is ' +
+            this.correctAnswer +
+            '.'
+          "
+        ></h4>
+        <button @click="this.getNewQuestion" class="send" type="button">
+          Next question
+        </button>
+      </section>
     </template>
-
-    <button @click="this.submitAnswer()" class="send" type="button">
-      Send
-    </button>
   </div>
 </template>
 
 <script>
+import ScoreBoard from "@/components/ScoreBoard.vue";
 export default {
   name: "App",
+  components: {
+    ScoreBoard,
+  },
 
   data() {
     return {
       question: undefined,
       incorrectAnswers: [],
       correctAnswer: undefined,
-      chosen_answer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: undefined,
     };
   },
   computed: {
@@ -45,27 +81,34 @@ export default {
   },
   methods: {
     submitAnswer() {
-      if (!this.chosen_answer) {
+      if (!this.chosenAnswer) {
         alert("pick one of the options");
       } else {
-        if (this.chosen_answer == this.correctAnswer) {
-          alert("You got it right!");
+        this.answerSubmitted = true;
+        if (this.chosenAnswer === this.correctAnswer) {
+          console.log("You got it right!");
         } else {
-          alert("You got it wrong");
+          console.log("You got it wrong");
         }
       }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+      this.axios
+        .get("https://opentdb.com/api.php?amount=1&category=18")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+          this.correctAnswer = response.data.results[0].correct_answer;
+          console.log(response.data.results[0]);
+        });
     },
   },
 
   created() {
-    this.axios
-      .get("https://opentdb.com/api.php?amount=1&category=18")
-      .then((response) => {
-        this.question = response.data.results[0].question;
-        this.incorrectAnswers = response.data.results[0].incorrect_answers;
-        this.correctAnswer = response.data.results[0].correct_answer;
-        console.log(response.data.results[0]);
-      });
+    this.getNewQuestion();
   },
 };
 </script>
